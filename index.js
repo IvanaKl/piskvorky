e32339205926fa33ff3cd4a7642813a654d175d1
Ivana Klementová <klementova.vf@gmail.com>
2021-04-13 10:44:26 +0200

    'ukol.4

--- index.js
+++ index.js
@@ -7,21 +7,25 @@ tlacitka.forEach((tlacitko) => {
   });
 });
 
-const kolecko = document.querySelector('#krizek');
-const krizek = document.querySelector('#kolecko');
+const koleckoHraje = document.querySelector('#krizek');
+const krizekHraje = document.querySelector('#kolecko');
 const buttons = document.querySelectorAll('button');
 
 for (let i = 0; i < buttons.length; i += 1) {
   const button = buttons[i];
 
   button.addEventListener('click', () => {
-    krizek.classList.toggle('hra-kroužek');
-    kolecko.classList.toggle('hra-krizek');
+    koleckoHraje.classList.toggle('board__field--cross');
+    document.querySelector('.hra-kroužek').style.display = 'inline-block';
 
-    if (kolecko.className === 'hra-krizek') {
-      button.innerHTML = `<img src="image2/cross-křízek.svg"/>`;
+    if (koleckoHraje.className === 'hra-krizek') {
+      document.querySelector('.hra-krizek').style.display = 'inline-block';
+      button.innerHTML = `<img  src="image2/cross-křízek.svg"/>`;
+      document.querySelector('.hra-krizek').style.display = 'none';
     } else {
-      button.innerHTML = `<img src="image2/circle-kruh.svg" />`;
+      document.querySelector('.hra-krizek').style.display = 'inline-block';
+      button.innerHTML = `<img  src="image2/circle-kruh.svg" />`;
+      document.querySelector('.hra-kroužek').style.display = 'none';
     }
   });
 }
